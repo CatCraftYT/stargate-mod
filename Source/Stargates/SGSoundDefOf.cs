44ffe06a85b2ce49d81ff8a37667f36620783415
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2022-07-27 22:40:08 +0930

    added SoundDefOf

---
+++ Source/Stargates/SGSoundDefOf.cs
@@ -0,0 +1,23 @@
+﻿using Verse;
+using RimWorld;
+
+namespace StargatesMod
+{
+    [DefOf]
+    public static class SGSoundDefOf
+    {
+        static SGSoundDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(SoundDefOf));
+        }
+
+        public static SoundDef StargateMod_SGOpen;
+        public static SoundDef StargateMod_SGFailDial;
+        public static SoundDef StargateMod_SGKawooshExplosion;
+        public static SoundDef StargateMod_SGIdle;
+        public static SoundDef StargateMod_SGClose;
+        public static SoundDef StargateMod_IrisOpen;
+        public static SoundDef StargateMod_IrisClose;
+        public static SoundDef StargateMod_IrisHit;
+    }
+}
